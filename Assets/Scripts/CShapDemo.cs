2122ac14808b159683a3652b8f6058fe6c8fa22d
DreamKnightKr <dreamknightkr@gmail.com>
2014-02-05 17:29:02 +0900

    C# <-> Boo로 구성된 Component 상호간에 값 전달 및 주고 받기 기본 예제 추가.

---
+++ Assets/Scripts/CShapDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CShapDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void CallFromHellBoo(string str)
+	{
+		Debug.Log("CallFromHellBoo");
+		Debug.Log(str);
+	}
+}
--- Assets/Scripts/GUIScript.cs
+++ Assets/Scripts/GUIScript.cs
@@ -2,14 +2,21 @@
 using System.Collections;
 
 public class GUIScript : MonoBehaviour {
+	Hashtable args = new Hashtable();
 
 	// Use this for initialization
 	void Start () {
-		}
+		args["Return"] = "";
+	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		if("" != (string)args["Return"])
+		{
+			Debug.Log("Get : " + args["Return"]);
+			args["Return"] = "";
+		}
 	}
 
 	void OnGUI()
@@ -19,17 +26,21 @@ public class GUIScript : MonoBehaviour {
 
 		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Hello Boo"))
 		{
+			// Unalble Directly Call
+			GetComponent("HelloBoo").SendMessage("PrintHelloBoo");
+			GetComponent("HelloBoo").SendMessage("PrintOnConsol", "HelloBoo");	// Send Parameter
 
 		}
 		nYPosCount++;
-		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Move Step1_Move"))
+		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Call C# Component"))
 		{
+			GetComponent("HelloBoo").SendMessage("SendMessageToCShapComponent");
 		}
 
 		nYPosCount++;
-		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Add Step1_Add"))
+		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "return From Boo"))
 		{
-
+			GetComponent("HelloBoo").SendMessage("ReturnValue", args);	// Boo will set value...
 		}
 	}
 }
