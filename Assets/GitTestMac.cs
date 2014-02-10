8c4bf74e3b02eada25d6179c3f4ec76aece7de68
DreamKnightMac <dreamknightkr@gmail.com>
2014-02-10 10:58:33 +0900

    컴포넌트 연결 Git Test Mac

---
+++ Assets/GitTestMac.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GitTestMac : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
